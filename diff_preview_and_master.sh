c5c8603f986126b9e27e92679f61d0a609d25e16
wangxuan-ones <wangxuan@ones.ai>
2022-12-12 11:28:23 +0800

    add diff_preview_and_master.sh

---
+++ diff_preview_and_master.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+source base.sh
+
+echo "检查master分支是否相比于版本preview分支有新增提交"
+
+for repo in ${repo_list[*]}; do
+    cd ${srcbase}/${repo}
+    echo "repo: ${repo} preview: ${preview_env} and master"
+    git --no-pager log --oneline origin/${preview_env}..origin/master
+done
+
+
