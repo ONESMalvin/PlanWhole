9ad260480245f7a0e6ee4bdd52e92a89ef8350df
wangxuan-ones <wangxuan@ones.ai>
2022-12-16 17:25:46 +0800

    optimize

--- preview_init.sh
+++ preview_init.sh
@@ -6,7 +6,7 @@ echo "推送版本到 preview"
 
 for repo in ${repo_list[*]}; do
     cd ${srcbase}/${repo}
-    echo "repo: ${repo} train:${train} preview: ${preview_env}"
+    echo "repo: ${GREEN}${repo}${NC} train:${RED}${train}${NC} preview: ${RED}${preview_env}${NC}"
     git flow train preview ${train} ${preview_env}
 done
 
