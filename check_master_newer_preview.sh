1943117d3e2fe0de630ea352d2d3dda8af15d078
wangxuan-ones <wangxuan@ones.ai>
2022-12-27 09:52:06 +0800

    fix update scripts

--- check_master_newer_preview.sh
+++ check_master_newer_preview.sh
@@ -2,24 +2,18 @@
 
 source base.sh
 
-for repo in ${repo_list[*]}; do
-    cd ${srcbase}/${repo}
-    echo "repo: ${repo}"
-    git log --oneline origin/master ^origin/${preview_env}
-done
-
 echo "检查master代码是否比preview代码有更新（一般在发版之前确认,看是否需要合入到preview给测试验证）"
 
 for repo in ${repo_list[*]}; do
     cd ${srcbase}/${repo}
     master_hash=`git rev-parse origin/master`
     preview_hash=`git rev-parse origin/${preview_env}`
-    lcount=`git --no-pager log --oneline origin/master..origin/${preview_env} | wc -l`
-    if [ "$lcount" != "0" ]; then
+    lcount=`git --no-pager log --oneline origin/${preview_env}..origin/master | wc -l`
+    if [ $lcount -le "0" ]; then
+        echo "${GREEN} repo: ${repo} master no newer ${NC}"
+    else 
         echo "${RED} repo: ${repo} master has updated ${NC}"
         echo master:$master_hash preview: $preview_hash
-    else 
-        echo "${GREEN} repo: ${repo} master no newer ${NC}"
     fi
 done
 
--- check_master_newer_train.sh
+++ check_master_newer_train.sh
@@ -8,12 +8,13 @@ for repo in ${repo_list[*]}; do
     cd ${srcbase}/${repo}
     master_hash=`git rev-parse master`
     train_hash=`git rev-parse train/${train}`
-    lcount=`git --no-pager log --oneline origin/master..origin/train/${train} | wc -l`
-    if [ "$lcount" != "0" ]; then
-        echo "${RED} repo: ${repo} master has updated ${NC}"
+    lcount=`git --no-pager log --oneline origin/train/${train}..origin/master | wc -l`
+    if [ $lcount -le 0 ]; then
+        echo "${GREEN} repo: ${repo} master no newer ${NC}"
         echo master:$master_hash train: $train_hash
     else 
-        echo "${GREEN} repo: ${repo} master no newer ${NC}"
+        echo "${RED} repo: ${repo} master has updated ${NC}"
+        echo master:$master_hash train: $train_hash
     fi
 done
 
